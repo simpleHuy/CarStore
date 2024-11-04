c3bd1319baf8de54361c533935e92c1cac3976eb
Sciderus <huydska11@gmail.com>
2024-11-04 22:05:23 +0700

    adjust OldPrice display

--- CarStore/Helpers/ConvertPriceToOldPrice.cs
+++ CarStore/Helpers/ConvertPriceToOldPrice.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,7 +14,9 @@ namespace CarStore.Helpers
         {
             var price = (long)value;
             price = price* 100 / 80;
-            return price.ToString();
+            CultureInfo vietnameseCulture = new CultureInfo("vi-VN");
+            var formattedAmount = price.ToString("C0", vietnameseCulture);
+            return formattedAmount;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, string language)
