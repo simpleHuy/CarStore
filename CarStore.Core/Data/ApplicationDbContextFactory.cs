d0c0ecc3d91fde45121f08e9d39ff06afc2c45ee
Sciderus <huydska11@gmail.com>
2024-11-20 21:50:44 +0700

    try to change .env path

--- CarStore.Core/Data/ApplicationDbContextFactory.cs
+++ CarStore.Core/Data/ApplicationDbContextFactory.cs
@@ -13,11 +13,9 @@ public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<Applicati
     {
         var basePath = AppContext.BaseDirectory;
         var curDir = new DirectoryInfo(basePath);
-        var corePath = curDir.Parent.Parent.Parent.Parent.Parent.Parent;
-        var envFile = Path.Combine(corePath.FullName, ".env");
+        var envFile = Path.Combine(curDir.FullName, ".env");
         DotNetEnv.Env.Load(envFile);
         var connectionString = Environment.GetEnvironmentVariable("CONNECTION_STRING");
-        Console.WriteLine(connectionString);
 
         if (string.IsNullOrEmpty(connectionString))
         {
